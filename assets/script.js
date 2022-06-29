cf0de446730d2afb1394c1c3d36494c670a4a932
Michael Nabatov <happyanddebtfree@gmail.com>
2022-06-29 11:40:31 -0400

    added functions to js file

--- assets/script.js
+++ assets/script.js
@@ -2,30 +2,34 @@ var topicsString = "HTML, CSS, Git, JavaScript";
 
 let topicsArray = ["HTML", "CSS", "Git", "JavaScript"];
 
-console.log(topicsString);
+function randomize (maxNbr){
+    return (Math.floor(Math.random() * maxNbr)+1);
+}
 
-for (i=0;i<topicsArray.length;i++){
-    console.log(topicsArray[i]);
+function listTopics(){
+    for (i=0;i<topicsArray.length;i++){
+        console.log(topicsArray[i]);
+    }
 }
 
 
-/*
-var topic = "CSS";
-
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function selectTopic(topic) {
+  if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
 }
-*/
 
-let shapes = ["circle","triangle","square","rectangle"];
-for (i=0;i<shapes.length;i++){
-    console.log(shapes[i]);
-}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+var n = randomize(topicsArray.length)-1;
+selectTopic(topicsArray[n]);
